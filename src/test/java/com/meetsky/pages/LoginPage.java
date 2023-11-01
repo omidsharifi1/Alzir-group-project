19a68bc051e7f416abdd6a2938f3b945de5965c5
omidsharifi1 <132533065+omidsharifi1@users.noreply.github.com>
2023-11-01 13:53:51 -0700

    Update LoginPage.java

--- src/test/java/com/meetsky/pages/LoginPage.java
+++ src/test/java/com/meetsky/pages/LoginPage.java
@@ -1,12 +1,11 @@
 package com.meetsky.pages;
 
-
 import com.meetsky.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends BasePage{
 
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
