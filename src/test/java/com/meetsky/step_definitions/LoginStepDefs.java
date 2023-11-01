c5ee804cc10da19048e2774f2875223ca16ac450
omidsharifi1 <132533065+omidsharifi1@users.noreply.github.com>
2023-11-01 13:51:11 -0700

    Update LoginStepDefs.java

--- src/test/java/com/meetsky/step_definitions/LoginStepDefs.java
+++ src/test/java/com/meetsky/step_definitions/LoginStepDefs.java
@@ -2,26 +2,29 @@ package com.meetsky.step_definitions;
 
 import com.meetsky.pages.LoginPage;
 import com.meetsky.utilities.ConfigurationReader;
-import com.meetsky.utilities.Driver;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
 
 public class LoginStepDefs {
 
     LoginPage loginPage = new LoginPage();
 
-    @When("User is already in the log in page")
-    public void user_is_already_in_the_log_in_page() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-    }
-    @Given("the user logged in with username as {string} and password as {string}")
-    public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
 
-        loginPage.login(username,password);
     }
 
+    @Then("User clicks on menu option")
+    public void userClicksOnMenuOption() {
+
+        loginPage.clickMenuByText("Files");
+    }
 
 
+    @Given("the user logged in with username and password")
+    public void theUserLoggedInWithUsernameAndPassword() {
 
-
+      loginPage.login(ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
+    }
 }
