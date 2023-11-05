f399163fae546b5fd58948cda77e1c2c8a42699f
ykgina031205 <enight10@gmail.com>
2023-11-05 09:24:22 -0700

    I deleted this page by mistake and redeployed it from the Master. It must be empty.

--- src/test/java/com/meetsky/pages/BasePage.java
+++ src/test/java/com/meetsky/pages/BasePage.java
@@ -1,7 +1,32 @@
 package com.meetsky.pages;
 
+import com.meetsky.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage {
+import java.util.List;
 
+public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li/a")
+    public List<WebElement> menuOptions;
+
+    public void clickMenuByText(String text){
+        try {
+            for (WebElement module : menuOptions) {
+                if (module.getAttribute("aria-label").equalsIgnoreCase(text)) {
+                    module.click();
+                }
+            }
+        }catch(Exception e){
+            System.err.println("No such element found with text: " + text);
+            e.printStackTrace();
+        }
+    }
 
 }
