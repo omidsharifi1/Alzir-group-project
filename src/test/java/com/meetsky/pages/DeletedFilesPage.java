9ce3f40db4a3cd1adf45299764b9c84e924ae220
ali <alijsaleem@gmail.com>
2023-11-07 22:12:54 -0800

    Jira feature file is updated

--- src/test/java/com/meetsky/pages/DeletedFilesPage.java
+++ src/test/java/com/meetsky/pages/DeletedFilesPage.java
@@ -1,11 +1,15 @@
 package com.meetsky.pages;
 
 import com.meetsky.utilities.Driver;
+import io.cucumber.java.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeletedFilesPage extends FilesPage {
@@ -25,7 +29,8 @@ public class DeletedFilesPage extends FilesPage {
     @FindAll({@FindBy(xpath = "((//table[@id='filestable'])[11]/tbody/tr/td[@class='date']/span[@data-timestamp])")})
     public List<WebElement> TimeElement;
 
-    @FindBy(xpath = "(//a[@class='action action-menu permanent'])[1]") // this is the location of the first file in the list
+    @FindBy(xpath = "(//a[@class='action action-menu permanent'])[1]")
+    // this is the location of the first file in the list
     public WebElement threeDots;
 
     @FindBy(linkText = "Delete permanently")
@@ -34,7 +39,70 @@ public class DeletedFilesPage extends FilesPage {
     @FindAll({@FindBy(xpath = "(//table[@id='filestable'])[11]/tbody/tr/td[@class='filename']//span[@class='nametext extra-data'] ")})
     public List<WebElement> Files;
 
+
+
+    @FindBy(linkText = "All files")
+    public WebElement AllFilesTab;
+
+    @FindAll({@FindBy(xpath = "(//table[@id='filestable'])[1]/tbody/tr/td//span[@class='innernametext']")})
+    public List<WebElement> FilesNamesInFilesTab;
+
+
+//    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @FindBy(xpath = "(//span[.=' Restore'])[1]")
+    public WebElement restoreButton;
+
+//================================================================================
+    public WebElement getWebElementByIndexToRestor(int index) {
+        String xpath = "(//span[.=' Restore'])[" + index + "]";
+        return Driver.getDriver().findElement(By.xpath(xpath));
+    }
+
+//============================================================================================
+
+
+//        List<String> FilesNames = new ArrayList<>();
+//
+//        // Populate FilesNames list with titles
+//        for (WebElement each : Files) {
+//            FilesNames.add(each.getAttribute("title"));
+//        }
+
+@BeforeClass
+public String getFirstDeletedByName() { // I use this method to store the name of first file in Deleted Files field
+
+    List<WebElement> xxxx = Files;
+
+    List<String> FilesNames = new ArrayList<>();
+
+    for (WebElement each : xxxx) {
+        FilesNames.add(each.getAttribute("title"));
+    }
+
+    if (FilesNames.isEmpty()) {
+        // Handle the case where the list is empty (e.g., return an appropriate default value)
+        return "No files found";
+    }
+
+    String fileName = FilesNames.get(0);
+    int dotIndex = fileName.lastIndexOf(".");
+    if (dotIndex > 0) {
+        fileName = fileName.substring(0, dotIndex);
+    }
+
+    return fileName;
+
+    }
+
+//=========================================================================================================
+
+
+
+//    ===================================================================================
 }
+
+
+
 
 
 
